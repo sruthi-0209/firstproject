4ea147684b115bbfddb8b557e85c3aab5b01070c
sruthi-0209 <106072925+sruthi-0209@users.noreply.github.com>
2022-05-24 12:28:46 +0530

    data3 variable is added

--- one.py
+++ one.py
@@ -1,4 +1,5 @@
 data1 = int(input("enter the number 1:"))
 data2 = int(input("enter the number 2:"))
-data3 = data1+data2
+data3 = int(input("enter the number 3:"))
+data3 = data1+data2+data3
 data3
